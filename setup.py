1c35855d129bc3987ac319a4c0bdb02e85c1061b
Administrator <hochikong@foxmail.com>
2017-10-16 20:14:22 +0800

    修复完成

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/9/1 22:33
+# @Author  : Hochikong
+# @Email   : hochikong@foxmail.com
+# @File    : publib.py
+# @Software: PyCharm
+
+from setuptools import setup, find_packages
+setup(
+    name='stockclib',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['neo4j-driver',
+                      'bs4',
+                      'jieba',
+                      'retrying',
+                      'requests',
+                      ],
+
+    description='The basic library for Sora',
+    author='Hochikong',
+    author_email='hochikong@foxmail.com',
+    url='http://github.com/hochikong'
+)
---
+++ stockclib/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/16 20:01
+# @Author  : Hochikong
+# @Email   : hochikong@foxmail.com
+# @File    : __init__.py.py
+# @Software: PyCharm
