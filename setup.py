19c41ca9673ae23701693144209110ea87cff98b
Administrator <hochikong@foxmail.com>
2017-10-19 19:59:53 +0800

    默认2个进程分词，增加了lxml的依赖安装

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
                       'jieba',
                       'retrying',
                       'requests',
+                      'lxml',
                       ],
 
     description='The basic library for Sora',
