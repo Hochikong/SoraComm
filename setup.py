4553376e6a8c1c0037ea8c337e13a54dfa64e109
Hochikong <ckhoidea@hotmail.com>
2017-12-21 00:53:26 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup, find_packages
 setup(
     name='stockclib',
-    version='0.3',
+    version='0.3.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['neo4j-driver',
